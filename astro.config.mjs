10d2ee04781a9b9ecafa409d504058763d209242
Ahmed Hassan <ah8438263@gmail.com>
2026-08-18 01:05:18 +0300

    Edit The app configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ahmed1b.github.io',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
